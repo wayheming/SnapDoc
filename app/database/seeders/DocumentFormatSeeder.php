4aa5e4ca6088dcebf63ece6a30734f585c32d43a
wayheming <way.h3ming@gmail.com>
2026-04-01 17:14:58 +0200

    feat: add DocumentFormatSeeder with real passport/visa formats

---
+++ app/database/seeders/DocumentFormatSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\DocumentFormat;
+use Illuminate\Database\Seeder;
+
+class DocumentFormatSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $formats = [
+            ['name' => 'UA Passport',        'country' => 'UA', 'width_mm' => 35, 'height_mm' => 45, 'dpi' => 300, 'is_active' => true, 'sort_order' => 1],
+            ['name' => 'International Pass.','country' => 'INT','width_mm' => 35, 'height_mm' => 45, 'dpi' => 300, 'is_active' => true, 'sort_order' => 2],
+            ['name' => 'ID Card UA',         'country' => 'UA', 'width_mm' => 25, 'height_mm' => 35, 'dpi' => 300, 'is_active' => true, 'sort_order' => 3],
+            ['name' => 'US Passport',        'country' => 'US', 'width_mm' => 51, 'height_mm' => 51, 'dpi' => 300, 'is_active' => true, 'sort_order' => 4],
+            ['name' => 'US Visa',            'country' => 'US', 'width_mm' => 51, 'height_mm' => 51, 'dpi' => 300, 'is_active' => true, 'sort_order' => 5],
+            ['name' => 'EU Passport',        'country' => 'EU', 'width_mm' => 35, 'height_mm' => 45, 'dpi' => 300, 'is_active' => true, 'sort_order' => 6],
+            ['name' => 'Schengen Visa',      'country' => 'EU', 'width_mm' => 35, 'height_mm' => 45, 'dpi' => 300, 'is_active' => true, 'sort_order' => 7],
+        ];
+
+        foreach ($formats as $format) {
+            DocumentFormat::firstOrCreate(
+                ['name' => $format['name'], 'country' => $format['country']],
+                $format
+            );
+        }
+    }
+}
